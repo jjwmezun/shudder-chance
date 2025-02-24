e0ecbe7236eb97c61dbcbd6d5eaea0c04234dab5
J. J. W. Mezun <jjwmezun@gmail.com>
2025-02-23 22:49:34 -0800

    Add header navigation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import * as React from 'react';
 
+const Header = () => {
+    return <header>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/poems">Poems</a></li>
+            </ul>
+        </nav>
+    </header>;
+};
+
 const App = props => {
     const { children } = props;
     return <html lang="en">
@@ -7,6 +18,7 @@ const App = props => {
             <link rel="stylesheet" href="/style.css" />
         </head>
         <body>
+            <Header />
             { children }
             <script src="/index.js"></script>
         </body>
